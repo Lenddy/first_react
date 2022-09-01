7cf1401d1d97bc6b06ad6de0bf092107e61b402b
Lenddy Morales <leme2003@gmail.com>
2022-08-31 21:51:50 -0400

    first react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello dojo</h1>
+      <h4>thing i need todo </h4>
+      <li>learn react</li>
+      <li>climb</li>
+      <li>feed the dog</li>
+      <li>run a marathon</li>
+    </div>
+  );
+}
+
+export default App;
